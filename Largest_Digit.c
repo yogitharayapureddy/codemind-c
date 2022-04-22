85d1079b3192de1910427d00d6e2fa3189d38a13
yogitharayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-04-22 20:08:26 +0530

    #include<stdio.h> int main() {     int n,i,temp=0;     scanf("%d",&n);     while(n>0)     {         i=n%10;         {             if(i>temp)             temp=i;         }n=n/10;
        }
        printf("%d",temp);
    }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
-int main ()
+int main()
 {
-int N, remider, Largest= 0;
-scanf ("%d", &N);
-
-while (N > 0)
+    int n,i,temp=0;
+    scanf("%d",&n);
+    while(n>0)
     {
-remider = N % 10;
-if (Largest < remider)
- {
-       Largest = remider;
- }
-N = N / 10;
+        i=n%10;
+        {
+            if(i>temp)
+            temp=i;
+        }n=n/10;
+        
     }
-printf ("%d", Largest);
-return 0;
+    printf("%d",temp);
 }
