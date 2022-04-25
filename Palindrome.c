ff3f32e07951c2560f3ac92fc671c08e5f336616
yogitharayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-04-25 18:50:45 +0530

    #include<stdio.h> int main() {     int n,i,temp,sum=0;     scanf("%d",&n);     temp=n;     while(n>0)     {         i=n%10;         sum=(sum*10)+i;         n=n/10;     }     if(sum==temp)     printf("True");     else     printf("False"); }

--- Palindrome.c
+++ Palindrome.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,sum=0,t;
+    int n,i,temp,sum=0;
     scanf("%d",&n);
-    t=n;
-    while(n!=0)
+    temp=n;
+    while(n>0)
     {
         i=n%10;
         sum=(sum*10)+i;
         n=n/10;
     }
-    if(sum==t)
+    if(sum==temp)
     printf("True");
     else
     printf("False");
-    return 0;
 }
