227d557c2f798035181d49225378fd8896d0466e
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-06-01 11:41:14 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d%d",&a,&b);     i=a>b?a:b;     while(1)     {         if(i%a==0&&i%b==0)         {             printf("%d",i);             break;         }         i++;     }     return 0; }

---
+++ LCM.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    scanf("%d%d%d",&a,&b);
+    i=a>b?a:b;
+    while(1)
+    {
+        if(i%a==0&&i%b==0)
+        {
+            printf("%d",i);
+            break;
+        }
+        i++;
+    }
+    return 0;
+}
