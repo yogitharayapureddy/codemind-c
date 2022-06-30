c038ce9e681f8909688ffac323ba6d0b96f72d52
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-06-30 13:26:46 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int n,c=0,i,a;     scanf("%d",&n);     int *arr;     arr=(int*)malloc(n*sizeof(int));     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<(n-1);i++)     {         if(arr[i-1]%2!=0 && arr[i+1]%2==0)         {             c++;         }         if(arr[i-1]%2==0 && arr[i+1]%2!=0)         {             c++;         }     }     printf("%d",c); }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,c=0,i,a;
+    scanf("%d",&n);
+    int *arr;
+    arr=(int*)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<(n-1);i++)
+    {
+        if(arr[i-1]%2!=0 && arr[i+1]%2==0)
+        {
+            c++;
+        }
+        if(arr[i-1]%2==0 && arr[i+1]%2!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
