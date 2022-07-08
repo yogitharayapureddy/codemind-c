731694a181939be25621bdc5c5c384a1d93b75eb
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-07-08 17:49:31 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     int a[n][m],i,j,o=0,e=0;     for(i=0;i<n;i++)     {         for (j=0;j<m;j++)         {             scanf("%d",&a[i][j]);             if(a[i][j]%2==0)             o+=a[i][j];             else             e+=a[i][j];         }     }     printf("%d %d",o,e); }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int a[n][m],i,j,o=0,e=0;
+    for(i=0;i<n;i++)
+    {
+        for (j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            if(a[i][j]%2==0)
+            o+=a[i][j];
+            else
+            e+=a[i][j];
+        }
+    }
+    printf("%d %d",o,e);
+}
