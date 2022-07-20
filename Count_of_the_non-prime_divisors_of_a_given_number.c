66c20e17e9648667e7387910bf545676ac031318
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-07-20 10:38:13 +0530

    #include<stdio.h> int prime(int num) {     int c=0,i=0;     for(i=1;i<=num;i++)     {         if(num%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     return 0; } int main() {     int a;     scanf("%d",&a);     int c=0,i;     for(i=1;i<a;i++)     {         if(a%i==0 && prime(i)==0)         {             c++;         }     }     printf("%d",c+1);
    }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int prime(int num)
+{
+    int c=0,i=0;
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int c=0,i;
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0 && prime(i)==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c+1);
+    
+}
