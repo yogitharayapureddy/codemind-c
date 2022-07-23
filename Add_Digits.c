cdc48b2332986966bac3c478d8e12a53f57f830c
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-07-23 08:34:10 +0530

    #include<stdio.h> int odd(int n) {     int r,s=0;     while(n)     {         r=n%10;         s=s+r;         n/=10;     }     if(s>=10)     {         return odd(s);     }     else     {         return s;     } } int main() {     int n;     scanf("%d",&n);     printf("%d",odd(n)); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
+int odd(int n)
+{
+    int r,s=0;
+    while(n)
+    {
+        r=n%10;
+        s=s+r;
+        n/=10;
+    }
+    if(s>=10)
+    {
+        return odd(s);
+    }
+    else
+    {
+        return s;
+    }
+}
 int main()
 {
-    int a,i=0,j;
-    scanf("%d",&a);
-    while(a>0||i>9)
-    {
-        if(a==0)
-        {
-            a=i;
-            i=0;
-        }
-        j=a%10;
-        i+=j;
-        a/=10;
-    }
-    printf("%d",i);
+    int n;
+    scanf("%d",&n);
+    printf("%d",odd(n));
 }
