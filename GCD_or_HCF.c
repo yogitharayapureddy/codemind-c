a9a877dd245eb366d3bde1ffa20db8ab9a1d0e1a
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-08-28 18:25:47 +0530

    #include<stdio.h> int main() {     int a,b,t,i=0;     scanf("%d%d",&a,&b);     for(t=2;t<=a&&t<=b;t++)     {         if(a%t==0 && b%t==0)         {             i=t;         }     }     if(i!=0)     printf("%d",i);     else     printf("1"); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,t,i=0;
+    scanf("%d%d",&a,&b);
+    for(t=2;t<=a&&t<=b;t++)
+    {
+        if(a%t==0 && b%t==0)
+        {
+            i=t;
+        }
+    }
+    if(i!=0)
+    printf("%d",i);
+    else
+    printf("1");
+}
