2df8afb3997f1766a65c7f48d7c6741cc9d6cec4
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-08-29 20:25:41 +0530

    #include<stdio.h> #include<math.h> int digit(int ); int main() {     int n,i,s=0,r;     scanf("%d",&n);     int b=n;     while(n!=0)     {         i=digit(n);         r=n%10;         s=s+pow(r,i);         n=n/10;     }     if (b==s)     printf("True");     else     printf("False"); } int digit(int a) {     int c=0;     while(a!=0)     {         a=a/10;         c++;     }     return c; }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,25 +1,30 @@
 #include<stdio.h>
 #include<math.h>
+int digit(int );
 int main()
 {
-    int a,n=0,t,i=0,j,u;
-    scanf("%d",&a);
-    t=a;
-    u=a;
-    while(a!=0)
+    int n,i,s=0,r;
+    scanf("%d",&n);
+    int b=n;
+    while(n!=0)
     {
-        a=a/10;
-        i++;
+        i=digit(n);
+        r=n%10;
+        s=s+pow(r,i);
+        n=n/10;
     }
-    while(t!=0)
-    {
-        j=t%10;
-        n=pow(j,i)+n;
-        t=t/10;
-        i--;
-    }
-    if(u==n)
+    if (b==s)
     printf("True");
     else
     printf("False");
+}
+int digit(int a)
+{
+    int c=0;
+    while(a!=0)
+    {
+        a=a/10;
+        c++;
+    }
+    return c;
 }
