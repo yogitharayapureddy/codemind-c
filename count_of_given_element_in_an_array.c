41dd9784da18ce4fc30900870b13596648cb6f19
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-09-07 15:21:51 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],s=0;     for(int i=00;i<n;i++)     {         scanf("%d",&a[i]);     }     int k;     scanf("%d",&k);     for(int i=n-1;i>=0;i--)     {         if(a[i]==k)         {             s++;         }     }     printf("%d",s); }

---
+++ count_of_given_element_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],s=0;
+    for(int i=00;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    for(int i=n-1;i>=0;i--)
+    {
+        if(a[i]==k)
+        {
+            s++;
+        }
+    }
+    printf("%d",s);
+}
