a94867f7091dddfa1c99e6739ad457077d43ae46
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-09-07 14:15:55 +0530

    #include<stdio.h> int prime(int n) {     if(n<=1)     return 0;     for(int i=2;i*i<=n;i++)     {         if(n%i==0)         return 0;     }     return 1; } int main() {     int n,r,s=0;     scanf("%d",&n);     int t=n;     while(n)     {         r=n%10;         s=s*10+r;         n/=10;     }     if(prime(t))     {         if(prime(s))         printf("circular prime");         else         printf("prime but not a circular prime");     }     else     printf("not prime");     return 0; }

---
+++ circular_prime.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int prime(int n)
+{
+    if(n<=1)
+    return 0;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n,r,s=0;
+    scanf("%d",&n);
+    int t=n;
+    while(n)
+    {
+        r=n%10;
+        s=s*10+r;
+        n/=10;
+    }
+    if(prime(t))
+    {
+        if(prime(s))
+        printf("circular prime");
+        else
+        printf("prime but not a circular prime");
+    }
+    else
+    printf("not prime");
+    return 0;
+}
