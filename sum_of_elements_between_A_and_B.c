cca5dc48c54ce3ccff5fe822d06a53fcc16a724e
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-08 11:25:49 +0530

    #include<stdio.h> int main() {     int i ,n,arr[100],sum=0,a,b;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i ,n,arr[100],sum=0,a,b;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
