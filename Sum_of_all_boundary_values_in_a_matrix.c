05f060927a97f1b47e36cf69c2bd378974d3ca8e
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-09 21:11:53 +0530

    #include<stdio.h> int main() {     int a[100][100],i,n,m,j,sum=0,max=0;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     for (i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if((i==0) || (i==n-1) || (j==0) || (j==m-1))             {                 sum=sum+a[i][j];             }         }     }     printf("%d",sum); }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],i,n,m,j,sum=0,max=0;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for (i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if((i==0) || (i==n-1) || (j==0) || (j==m-1))
+            {
+                sum=sum+a[i][j];
+            }
+        }
+    }
+    printf("%d",sum);
+}
