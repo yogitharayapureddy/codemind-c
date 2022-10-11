6f1e3e3a21336e93c07e53d7a1b87405be29e6b3
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-11 21:18:07 +0530

    #include <stdio.h> int main() {      int a[100][100],i,n,m,j,sum=0;      scanf("%d%d",&n,&m);      for(i=0;i<n;i++)      {           for(j=0;j<m;j++)           {                scanf("%d",&a[i][j]);           }      }      for(i=0;i<n;i++)      {         sum=0;         for(j=0;j<m;j++)         {             sum=sum+a[i][j];         }         printf("%d ",sum);      } }

--- Row_wise_sum_of_a_matrix.c
+++ Row_wise_sum_of_a_matrix.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a[100][100],i,n,m,j,sum=0;
-    scanf("%d%d",&n,&m);
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<m;j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
-    }
-    for(i=0;i<n;i++)
-    {
+     int a[100][100],i,n,m,j,sum=0;
+     scanf("%d%d",&n,&m);
+     for(i=0;i<n;i++)
+     {
+          for(j=0;j<m;j++)
+          {
+               scanf("%d",&a[i][j]);
+          }
+     }      
+     for(i=0;i<n;i++)
+     {
         sum=0;
         for(j=0;j<m;j++)
         {
-            sum=sum+a[i][j];
+            sum=sum+a[i][j]; 
         }
         printf("%d ",sum);
-    }
+     }      
 }
