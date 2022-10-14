c5bb6c1fe5b23bdabb87c2cf6656cf47e2611404
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-14 20:49:49 +0530

    #include<stdio.h> int main() {   int n, a[900],i,c=0;   scanf("%d",&n);   for(i =0;i<n;i++)   {     scanf("%d",&a[i]);   }
      for( i=1; i<n-1; i++)
      {
        if (a[i-1]%2==0 && a[i+1]%2==0 && a[i]%2)
        {
            c++;
        }
      }
      printf("%d",c);
    }

--- count_odd_numbers_between_even_numbers.c
+++ count_odd_numbers_between_even_numbers.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,c=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+  int n, a[900],i,c=0;
+  scanf("%d",&n);
+  for(i =0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+
+  for( i=1; i<n-1; i++)
+  {
+    if (a[i-1]%2==0 && a[i+1]%2==0 && a[i]%2)
     {
-        scanf("%d",&a[i]);
+        c++;
     }
-    for(i=1;i<(n-1);i++)
-    {
-        if(a[i-1]%2==0 && a[i+1]%2==0)
-        {
-            if(a[i]%2!=0)
-            {
-                c++;
-            }
-        }
-    }
-    printf("%d",c);
+  }
+  printf("%d",c);
 }
