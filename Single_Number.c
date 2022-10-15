d0bb5f5752c16d239aebde3ca0750b58c9c6e897
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-15 23:06:08 +0530

    #include<stdio.h> int main() {     int x[1000],i,n,j,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         flag=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(x[i]==x[j])                 {                     flag=1;                     break;                 }             }         }         if(flag==0)         {             printf("%d",x[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int x[1000],i,n,j,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        flag=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(x[i]==x[j])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag==0)
+        {
+            printf("%d",x[i]);
+        }
+    }
+}
