20ffecea263deaefa56f64e22e78743af9cdca62
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-15 20:30:06 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],dc=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]==1 || arr[i]==0)         {             dc++;         }     }     if(dc==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Binary_Array.c
+++ Binary_Array.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,k=0,i;
+    int n,i,arr[100],dc=0;
     scanf("%d",&n);
-    int a[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-        if(a[i]!=0 and a[i]!=1)
-        k=1;
-
+        scanf("%d",&arr[i]);
     }
-    if(k==0)
-    printf("True");
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==1 || arr[i]==0)
+        {
+            dc++;
+        }
+    }
+    if(dc==n)
+    {
+        printf("True");
+    }
     else
-    printf("False");
+    {
+        printf("False");
+    }
 }
